73354fcc8e9066c46cd7dff8bbe3b28d132af358
Maksim <75714743+Maksim8811@users.noreply.github.com>
2023-04-05 21:14:53 +0300

    add style for Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,29 +1,29 @@
-
+import css from "./Profile.module.css"
 import PropTypes from "prop-types"
 export const Profile = ({username,tag,location,avatar,stats}) => {
     
-    return (<div className="profile">
-  <div className="description">
+    return (<div className={css.profile}>
+  <div className={css.description}>
     <img
       src={avatar}
       alt="User avatar"
-      className="avatar"
+      className={css.avatar}
     />
-    <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-    <p className="location">{location}</p>
+    <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+    <p className={css.location}>{location}</p>
   </div>
 
-  <ul className="stats">
-    <li>
+  <ul className={css.stats}>
+    <li className={css.stats_list}>
       <span className="label">Followers</span>
       <span className="quantity">{stats.followers}</span>
     </li>
-    <li>
+    <li className={css.stats_list}>
       <span className="label">Views</span>
       <span className="quantity">{stats.views}</span>
     </li>
-    <li>
+    <li className={css.stats_list}>
       <span className="label">Likes</span>
       <span className="quantity">{stats.likes}</span>
     </li>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,27 +1,23 @@
-// import PropTypes from "prop-types"
+import PropTypes from "prop-types"
 
 export const Statistics = ({title, stats}) => {
-    console.log(title)
    return ( <section className="statistics">
     {title.length > 0 && <h2 className="title">{title}</h2>}
   
     <ul className="stat-list">
-    <li className="item">
-      <span className="label">.docx</span>
-      <span className="percentage">4%</span>
-    </li>
-    <li className="item">
-      <span className="label">.mp3</span>
-      <span className="percentage">14%</span>
-    </li>
-    <li className="item">
-      <span className="label">.pdf</span>
-      <span className="percentage">41%</span>
-    </li>
-    <li className="item">
-      <span className="label">.mp4</span>
-      <span className="percentage">12%</span>
-    </li>
+    {stats.map(stat => {
+        console.log(stat.id)
+        return (
+        <li key={stat.id} className="item">
+        <span className="label">{stat.label}</span>
+        <span className="percentage">{stat.percentage}</span>
+      </li>
+    )}
+        )}
   </ul>
 </section>
 )}
+
+Statistics.propTypes = {
+    stats:  PropTypes.array
+    }
